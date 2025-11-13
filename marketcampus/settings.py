2ad44c5c81d439a8e5f939cc84d6fa4d1357d5cb
Mateo <dddmateo@gmail.com>
2025-11-12 19:45:59 -0500

    hola

--- marketcampus/settings.py
+++ marketcampus/settings.py
@@ -63,6 +63,7 @@ WSGI_APPLICATION = 'marketcampus.wsgi.application'
 # Base de datos (puedes usar SQLite o configurar PostgreSQL en Render)
 DATABASES = {
     'default': {
+        
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
